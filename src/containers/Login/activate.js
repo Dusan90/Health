4be21251820da5fdd8e16af2cf7c64d24f0f803c
Dusan <ducati.duka@gmail.com>
2020-05-05 19:37:06 +0200

    login verification

--- src/containers/Login/activate.js
+++ src/containers/Login/activate.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export class activate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.match.params.id,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://healthcarebackend.xyz/api/auth/activate/${this.state.id}`)
+      .then((response) => {
+        console.log(response);
+      });
+  }
   render() {
+    console.log(this.state.id);
+
     return <div>hello</div>;
   }
 }
