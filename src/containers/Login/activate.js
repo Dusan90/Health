b7c3d6c6bba0c445878f2365a10940addc32bef5
Dusan <ducati.duka@gmail.com>
2020-05-06 18:26:51 +0200

    login verification

--- src/containers/Login/activate.js
+++ src/containers/Login/activate.js
@@ -10,39 +10,45 @@ export class activate extends Component {
   }
 
   getResponse = async () => {
-    // axios
-    //   .get(`https://healthcarebackend.xyz/api/auth/activate/${this.state.id}`)
-    //   .then((response) => {
-    //     console.log(response.data, "sta je bre ovo");
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-
-    var altPattern =
-      "(?=^.{8,}$)((?=.*d)|(?=.*W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"; // using quotes and new RegEx() syntax...
-    var regex = new RegExp(altPattern);
-    var str = this.state.id;
-    if (str.match(altPattern) != null && regex.test(str) != null) {
+    const mediumRegex = new RegExp(
+      "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"
+    );
+    if (mediumRegex.test(this.state.id)) {
       axios
-        .get(`https://healthcarebackend.xyz/api/auth/activate/${str}`)
+        .get(`https://healthcarebackend.xyz/api/auth/activate/${this.state.id}`)
         .then((response) => {
           console.log(response.data, "sta je bre ovo");
         })
         .catch((error) => {
           console.log(error);
         });
+      console.log("come on");
     } else {
-      alert("invalid");
+      console.log("nzm");
     }
+
+    // var altPattern =
+    //   "(?=^.{8,}$)((?=.*d)|(?=.*W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"; // using quotes and new RegEx() syntax...
+    // var regex = new RegExp(altPattern);
+    // var str = this.state.id;
+    // if (str.match(altPattern) != null && regex.test(str) != null) {
+    //   axios
+    //     .get(`https://healthcarebackend.xyz/api/auth/activate/${str}`)
+    //     .then((response) => {
+    //       console.log(response.data, "sta je bre ovo");
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //     });
+    // } else {
+    //   alert("invalid");
+    // }
   };
 
   componentDidMount() {
     this.getResponse();
   }
   render() {
-    console.log(this.props);
-
     return <div>hello</div>;
   }
 }
