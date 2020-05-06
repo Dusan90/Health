30d1b138693eb357d2811356504693ed00637448
Dusan <ducati.duka@gmail.com>
2020-05-06 17:14:32 +0200

    login verification

--- src/containers/Login/activate.js
+++ src/containers/Login/activate.js
@@ -18,6 +18,23 @@ export class activate extends Component {
       .catch((error) => {
         console.log(error);
       });
+
+    var altPattern =
+      "(?=^.{8,}$)((?=.*d)|(?=.*W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"; // using quotes and new RegEx() syntax...
+    var regex = new RegExp(altPattern);
+    var str = this.state.id;
+    if (str.match(altPattern) != null && regex.test(str) != null) {
+      axios
+        .get(`https://healthcarebackend.xyz/api/auth/activate/${this.state.id}`)
+        .then((response) => {
+          console.log(response.data, "sta je bre ovo");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      alert("invalid");
+    }
   };
 
   componentDidMount() {
