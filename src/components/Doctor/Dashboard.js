2ec7625a325c5c3e59c36461cbbdf59035416d61
Dusan <ducati.duka@gmail.com>
2020-06-18 17:36:27 +0200

    push test

--- src/components/Doctor/Dashboard.js
+++ src/components/Doctor/Dashboard.js
@@ -100,6 +100,7 @@ const Dashboard = ({
       <div className="dashboardIcon">
         <FaFileAlt className="dashIcon" />
         <h2>Dashboard</h2>
+        <h1>test</h1>
       </div>
       <div className="main">
         {" "}
